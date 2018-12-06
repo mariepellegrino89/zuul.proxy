1fd7d28a1fbe26b6f286cc5fafe0f9b5eb2c4616
Mario Pellegrino <mario.pellegrino@spindox.it>
2018-12-06 15:51:24 +0100

    first git uul com

--- src/main/java/spring/boot/zuul/proxy/Application.java
+++ src/main/java/spring/boot/zuul/proxy/Application.java
@@ -2,7 +2,9 @@ package spring.boot.zuul.proxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class Application {
 
